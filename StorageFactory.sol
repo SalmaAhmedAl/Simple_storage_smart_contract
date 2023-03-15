0489707395290bf26ea69104a5b359478495de98
SalmaAhmed.Z <56123892+SalmaAhmedAl@users.noreply.github.com>
2023-03-15 07:36:13 +0200

    importing contract in another contract

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,7 +1,8 @@
-// SPDX-Lisence-Identifier: MIT
+//SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.7;
 
+//Just when you import that contract, you get the ABI
 import "./SimpleStorage.sol"; 
 
 contract StorageFactory {
@@ -13,15 +14,21 @@ contract StorageFactory {
         simpleStorageArray.push(simpleStorage);
     }
     
-    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageFavNumber) public {
         // Address 
-        // ABI 
-        // SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
-        simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
+        // ABI - Application Binary Interface >>Look at Compilation details/ABI (tells you all inputs, outputs and everything you could do with this contract)
+       
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        simpleStorage.store(_simpleStorageFavNumber);
+        
+        //short way
+        //simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
     }
     
     function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
-        // return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
         return simpleStorageArray[_simpleStorageIndex].retrieve();
+        //short way
+        // return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
+        
     }
 }
